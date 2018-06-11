ea82fd4584517b3b10b0eb127e16944bea32c8da
Pia Soy <piasoy4@gmail.com>
2018-06-11 04:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0){
-   return ('The line is currently empty')
+   return ('The line is currently empty.')
  }
  else{
    for (var i = 0; i < katzDeliLine.length; i++){
