bc5e7e786b8b55fe07259e8b5e17ffc4a214a6e2
Pia Soy <piasoy4@gmail.com>
2018-06-11 04:55:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ function currentLine(katzDeliLine){
    return ('The line is currently empty')
  }
  else{
-   return (`The line is currently ${i+1}. ${katzDeliLine[i]}`)
+   for (var i = 0, i < katzDeliLine.length, i++){
+     katzDeliLine.push(`${i++}. ${katzDeliLine[i]}`)
+   }
+   return (`The line is currently ${katzDeliLine}`)
  }
 }
   
